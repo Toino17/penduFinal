c41d0a87577b976ea50555629e6a66065e28b664
Toino17 <colonnact@outlook.com>
2023-09-28 00:51:33 +0200

    final

--- pendu.js
+++ pendu.js
@@ -21,7 +21,6 @@ let data = [
             "absurdement",
             "absurdité",
             "absurdités",
-            "abuser de",
             "académie",
             "académique",
             "acceptant",
@@ -45,18 +44,22 @@ let playbutton3 = document.querySelector(".playButton3")
 let UnderGuess = ""
 let baliseHistory = document.querySelector(".historyP")
 let historia = "Historique: "
-let reg = /^[a-zA-Zé]$/
+let reg = /^[a-zA-Zé]+$/
 let attempt=1
 let alreadyUse = 0
 let erreur = document.querySelector(".erreur")
 let balisedevineP = document.querySelector(".devineP")
 let baliseDevine = document.querySelector(".devineP")
-const tryWord = document.querySelector(".tryWord")
+const replayButton = document.querySelector('.replay')
+let tryWord = document.querySelector(".tryWord")
+let valueInputGuess = "??"
+
 
 function affiche (){
     baliseHistory.style.visibility = "visible"
     balisedevineP.style.visibility = "visible"
     playbutton2.style.visibility = "visible"
+    tryWord.style.visibility = "visible"
 }
 
 function searchRandomWord(){
@@ -75,10 +78,12 @@ function dataToUnder(randomWord){
 }
 
 function ShowUnder(){
-    baliseDevine.style.letterSpacing = "20px"
+    baliseDevine.style.letterSpacing= "20px"
+    baliseDevine.style.paddingLeft = "20px"
     baliseDevine.innerHTML = UnderGuess
     if (UnderGuess.toLowerCase()==randomWord){
         baliseDevine.style.color = "green"
+        animeLetter()
     }
 }
 
@@ -104,18 +109,34 @@ function history(){
 function reset(){
     baliseHistory.innerHTML = "Historique: "
     historia = "Historique: "
-        for (attempt=2 ; attempt <= 11; attempt++) {
-        document.getElementById(`rect${attempt}`).style.display = "none"   
+    for (attempt=2 ; attempt <= 11; attempt++) {
+        document.getElementById(`rect${attempt}`).style.display = "none"
     }
     attempt = 1
     baliseDevine.style.color = "black"
-    tryWord.style.visibility = "hidden" 
+    erreur.style.visibility = 'hidden'
+}
+
+function playtoReplay(){
+    replayButton.style.display = "flex"
+    playbutton.remove()
 }
 
 
 
+replayButton.addEventListener("click", function(event) {
+    event.preventDefault()
+    affiche()
+    reset()
+    searchRandomWord()
+    dataToUnder(randomWord)
+    ShowUnder()
+    console.log('value:', valueInputGuess)
+  }); 
+
 playbutton.addEventListener("click", function(event) {
     event.preventDefault()
+    playtoReplay()
     affiche()
     reset()
     searchRandomWord()
@@ -126,25 +147,50 @@ playbutton.addEventListener("click", function(event) {
   playbutton3.addEventListener("click", function(event) {
     event.preventDefault()
     let inputGuess = document.querySelector(".tryInput")
-    let valueInputGuess = inputGuess.value.toLowerCase()
+    valueInputGuess = inputGuess.value.toLowerCase()
     if (valueInputGuess==randomWord){
         baliseDevine.style.color = "green"
-
+        baliseDevine.innerHTML = randomWord.charAt(0).toUpperCase() + randomWord.slice(1);
+        animeLetter();
     }
-    // creatediv.classList.add('b');
-    // creatediv.id = 'presentation';
-    // document.body.appendChild(creatediv);
+    else if (valueInputGuess =="" || reg.test(valueInputGuess)!==true ) {
+        let erreurPropal = document.querySelector('.erreurPropal')
+        erreurPropal.style.display = 'block'
+        return
+    }
+    else{
+        alert("Vous avez perdu!")
+        baliseDevine.style.color = "red"
+        baliseDevine.innerHTML = randomWord.charAt(0).toUpperCase() + randomWord.slice(1);
+    }
+    document.getElementById("premierPlan").classList.remove("show")
+    document.getElementById("arrierePlan").style.display = "none";
+    document.getElementById("premierPlan").style.display = "none";
+    
+    return valueInputGuess = randomWord
+    
   }); 
-  let modal = document.querySelector("#premierPlan")
-console.log(modal.classList.contains("show"))
+
+
+let modal = document.querySelector("#premierPlan")
 
 function listenerKey(){
     document.addEventListener("keydown",function(event){
+        if (attempt == 11) {
+            return
+        }
+        if (UnderGuess.toLowerCase()==randomWord) {
+            return
+        }
         if (modal.classList.contains("show")) {
             return
         }
-        console.log(event.target)
-        if (reg.test(event.key) == true ) {
+        if (valueInputGuess==randomWord) {
+            return
+        }
+        console.log(event.key)
+        console.log(reg.test(event.key))
+        if (reg.test(event.key) == true && event.key.length ==1) {
             caracKey = (event.key.toLowerCase())
             history()
             let pos = randomWord.indexOf(caracKey)
@@ -163,6 +209,13 @@ function listenerKey(){
             else if (alreadyUse==0){
                 attempt++
                 document.getElementById(`rect${attempt}`).style.display = "block"
+                console.log(attempt);
+                if (attempt == 11) {
+                    baliseDevine.style.color = "red"
+                    baliseDevine.innerHTML = randomWord.charAt(0).toUpperCase() + randomWord.slice(1);
+                    alert('Vous avez perdu.')
+                    return UnderGuess = baliseDevine.innerHTML
+                }
             }
             else{
                 return
@@ -172,7 +225,7 @@ function listenerKey(){
             
             ShowUnder()
             if (UnderGuess.toLowerCase()==randomWord){
-                
+                return UnderGuess
             }
     }   }   )
     
@@ -187,12 +240,18 @@ function listenerKey(){
 listenerKey()
 
 
+
+
 function Modal() {
+    console.log(randomWord)
+    console.log(UnderGuess.toLowerCase())
+    if (UnderGuess.toLowerCase()==randomWord || valueInputGuess == randomWord){
+        return
+    }
     document.getElementById("arrierePlan").style.display = "block";
     document.getElementById("premierPlan").style.display = "block";
     document.getElementById("premierPlan").classList.add("modalContentPerso", "show")
-    console.log(modal)
-    let cancelButtons = document.querySelectorAll(".close, .containerBgOpacity, .playButton3");
+    let cancelButtons = document.querySelectorAll(".close, .containerBgOpacity");
         cancelButtons.forEach(function (button) {
             button.addEventListener("click", function () {
                 document.getElementById("premierPlan").classList.remove("show")
@@ -200,8 +259,28 @@ function Modal() {
                 document.getElementById("premierPlan").style.display = "none";
             });
         })
-        console.log(modal.classList.contains("show"))
 };
+
+function animeLetter(){
+    balisedevineP.innerHTML = balisedevineP.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    let anim = document.querySelectorAll('.letter')
+    let delay = 50
+    anim.forEach(element => {element.animate(
+        [
+          { transform: "translateY(0px)" },
+          { transform: "translateY(-10px)" },
+        ],
+        {
+          duration: 300,
+          delay: delay,
+          iterations: 1,
+          
+        },
+      );
+      delay+=80;
+    })
+}
 
 
 // document.body.style.backgroundColor="red"
+
